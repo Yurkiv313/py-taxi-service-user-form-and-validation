46489c7ee81d413570f329f5d41bf5dbbf047846
Andrii <yurkivandriy02@gmail.com>
2025-04-08 09:22:12 +0300

    add absolute import

--- taxi/views.py
+++ taxi/views.py
@@ -4,8 +4,8 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarCreationForm
-from .models import Driver, Car, Manufacturer
+from taxi.forms import DriverCreationForm, DriverLicenseUpdateForm, CarCreationForm
+from taxi.models import Driver, Car, Manufacturer
 
 
 @login_required
